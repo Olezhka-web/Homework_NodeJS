9b744d10b2059bbfc52a0a863cf262c0c861cff8
Olezhka-web <zcegthrkfcghj5@gmail.com>
2021-08-18 16:28:32 +0300

    Update

--- app.js
+++ app.js
@@ -1,51 +1,34 @@
 // Task 1
-// const fs = require('fs');
-// const path = require('path');
-//
-// const dirPath1 = path.join(__dirname, '/1800');
-// const dirPath2 = path.join(__dirname, '/2000');
-//
-// function readDir(dirPath1, dirPath2){
-//     fs.readdir(dirPath1, (err, files) => {
-//         files.forEach(fileName => {
-//             fs.readFile(path.join(dirPath1, fileName), ((err, data) => {
-//                 if (err) {
-//                     console.log(err);
-//                     return;
-//                 }
-//                 const user = JSON.parse(data);
-//                 if(user.gender === 'male'){
-//                     fs.rename(path.join(dirPath1, fileName), path.join(dirPath2, fileName), err2 => {
-//                         if (err2) {
-//                              console.log(err2);
-//                         }
-//                     });
-//                 }
-//             }));
-//         });
-//     });
-//
-//     fs.readdir(dirPath2, (err, files) => {
-//         files.forEach(fileName => {
-//             fs.readFile(path.join(dirPath2, fileName), ((err, data) => {
-//                 if (err) {
-//                     console.log(err);
-//                     return;
-//                 }
-//                 const user = JSON.parse(data);
-//                 if(user.gender === 'female'){
-//                     fs.rename(path.join(dirPath2, fileName), path.join(dirPath1, fileName), err2 => {
-//                         if (err2) {
-//                             console.log(err2);
-//                         }
-//                     });
-//                 }
-//             }));
-//         });
-//     });
-// }
-//
-// readDir(dirPath1, dirPath2);
+const fs = require('fs');
+const path = require('path');
+
+const dirPath1 = path.join(__dirname, '/1800');
+const dirPath2 = path.join(__dirname, '/2000');
+
+
+const readDir = (dirPath1, dirPath2, genderName) =>{
+    fs.readdir(dirPath1, (err, files) => {
+        files.forEach(fileName => {
+            fs.readFile(path.join(dirPath1, fileName), ((err, data) => {
+                if (err) {
+                    console.log(err);
+                    return;
+                }
+                const user = JSON.parse(data);
+                if(user.gender === genderName){
+                    fs.rename(path.join(dirPath1, fileName), path.join(dirPath2, fileName), err2 => {
+                        if (err2) {
+                            console.log(err2);
+                        }
+                    });
+                }
+            }));
+        });
+    });
+}
+
+readDir(dirPath1, dirPath2, 'male');
+readDir(dirPath2, dirPath1, 'female');
 
 // Task 2
 // const fs = require('fs');
