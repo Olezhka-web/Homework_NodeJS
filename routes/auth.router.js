3246e2257aa325fc0b21426125c7aab3cfbda5d1
Olezhka-web <71943430+Olezhka-web@users.noreply.github.com>
2021-08-26 14:56:51 +0300

    Delete auth.router.js

--- routes/auth.router.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-
-const { authController } = require('../controllers');
-
-router.get('/login', authController.loginPage);
-router.post('/login', authController.loginCheck);
-
-module.exports = router;
